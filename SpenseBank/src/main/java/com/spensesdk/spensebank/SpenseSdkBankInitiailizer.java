ea1d829f20ae7423f3881a4066a2a174851a8794
Prajjwal <prajjwal@spense.money>
2023-09-05 18:37:19 +0530

    first commit

---
+++ SpenseBank/src/main/java/com/spensesdk/spensebank/SpenseSdkBankInitiailizer.java
@@ -0,0 +1,12 @@
+package com.spensesdk.spensebank;
+
+import android.content.Context;
+
+public class SpenseSdkBankInitiailizer {
+
+    Context context;
+
+    public SpenseSdkBankInitiailizer(Context context) {
+        this.context = context;
+    }
+}
